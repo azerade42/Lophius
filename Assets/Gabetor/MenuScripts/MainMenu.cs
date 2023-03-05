42819f1a73024e41ea4907f727b50eeca6604d9b
Avery Carpenter <avercarpenter@gmail.com>
2023-03-05 12:37:46 -0500

    Menu Marshy Bite
    added menu animation

--- Assets/Gabetor/MenuScripts/MainMenu.cs
+++ Assets/Gabetor/MenuScripts/MainMenu.cs
@@ -5,10 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+ //   public GameObject Angler; 
+
+    private float delayBeforeLoading = 1.7f; 
+    [SerializeField]
+   // private string sceneToLoad; 
+
+    private float timeElapsed;
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime; 
+        if(timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       // animator.SetTrigger("Bite");
+        
+       //yield return new WaitForSeconds(1);
+      // StartCoroutine(ButtonDelay());
+
+       // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
+
+    // IEnumerator ButtonDelay()
+    // {
+    //  print(Time.time);
+    //  yield return new WaitForSeconds(30);
+    //  print(Time.time);
+
+
+    // }
 
     public void QuitGame()
     {
