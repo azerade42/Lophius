a3a11295d6900b27b80363906512c160ee3ccb1e
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-03-05 22:28:28 -0500

    Fixed Pause Menu

--- Assets/Gina/Scripts/PauseUI.cs
+++ Assets/Gina/Scripts/PauseUI.cs
@@ -12,7 +12,7 @@ public class PauseUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !GameIsPaused)
         {
             Cursor.lockState = CursorLockMode.None;
             if (GameIsPaused)
