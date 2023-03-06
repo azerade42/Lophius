d6aee5ca151cadc736b1a7dcc6cee5e49bd421b5
Avery Carpenter <avercarpenter@gmail.com>
2023-03-05 22:43:51 -0500

    Revert "Revert "Merge branch 'main' into Avery2""
    This reverts commit 0a958290693d298fe3d5ed7e5884ba7a38f56c68.

---
+++ Assets/PlayBiteAnim.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBiteAnim : MonoBehaviour
+{
+
+    // public Animator animator; 
+    // public Button play; 
+    // public Canvas canvas; 
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     play = play.GetComponent<Button>();
+
+    // }
+ 
+  
+    // public void Press() 
+      
+    // {
+    //  play.enabled = true;
+    //  //AudioSource.PlayClipAtPoint(sound, transform.position);
+    //  animator.SetTrigger("Bite");
+     
+ 
+    // }
+}
